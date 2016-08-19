3f6056cb606976db2d6335da075baac077c6a972
jithinbp <jithinbp@gmail.com>
2016-08-19 11:57:20 +0530

    linear ccd app

--- seel_res/GUI/E_MISCELLANEOUS/B/Z_LinearCCD.py
+++ seel_res/GUI/E_MISCELLANEOUS/B/Z_LinearCCD.py
@@ -60,10 +60,14 @@ class AppWindow(QtGui.QMainWindow, template_graph_nofft.Ui_MainWindow,utilitiesC
 		
 		self.WidgetLayout.setAlignment(QtCore.Qt.AlignLeft)
 		self.ControlsLayout.setAlignment(QtCore.Qt.AlignRight)
+		self.addRegularButton(self.ControlsLayout,self.setRef,'Set reference')
+		self.addRegularButton(self.ControlsLayout,self.resetRef,'Reset reference')
+		self.absorbance = self.addPauseButton(self.ControlsLayout,self.calcAbsorbance)
+		self.absorbance.setText('Absorbance')
 		self.ControlsLayout.addWidget(self.setStateIcon(I=self.I))
 
 		#Utility widgets
-		self.I.set_pv1(4)
+		#self.I.set_pv1(4)
 
 		#Control widgets
 		a1={'TITLE':'SH','MIN':2,'MAX':10,'FUNC':self.set_timebase,'UNITS':'S','TOOLTIP':'Set SH pulse width, and timebase'}
@@ -79,15 +83,20 @@ class AppWindow(QtGui.QMainWindow, template_graph_nofft.Ui_MainWindow,utilitiesC
 		a1={'TITLE':'ICG','MIN':1,'MAX':65000,'FUNC':self.set_icg,'UNITS':'S','TOOLTIP':'Set ICG'}
 		self.WidgetLayout.addWidget(self.dialIcon(**a1))
 
-		self.I.set_sine2(1000)
 
 		self.running=True
 		self.fit = False
 		self.timer.singleShot(100,self.run)
 		self.Y = np.zeros(3648)
+		self.REFY = np.zeros(3648)
+		self.setRefFlag = False
 		self.num = 0
 
 
+	def setRef(self):
+		self.setRefFlag = True
+	def resetRef(self):
+		self.REFY = np.zeros(3648)
 
 
 	def set_tweak(self,g):
@@ -102,15 +111,15 @@ class AppWindow(QtGui.QMainWindow, template_graph_nofft.Ui_MainWindow,utilitiesC
 		self.icg = g
 		return g*1e-6
 
-
+	def calcAbsorbance(self):
+		pass
+		
 	def autoRange(self):
 		xlen = 3648
 		self.plot.autoRange();
 		chan = self.I.analogInputSources[self.chan]
-		R = [chan.calPoly10(0),chan.calPoly10(1023)]
-		R[0]=R[0]*.9;R[1]=R[1]*1.1
-		self.plot.setLimits(yMax=max(R),yMin=min(R),xMin=0,xMax=xlen)
-		self.plot.setYRange(min(R),max(R))			
+		self.plot.setLimits(yMax=3.3,yMin=-3.3,xMin=0,xMax=xlen)
+		self.plot.setYRange(-3.3,3.3)			
 		self.plot.setXRange(0,xlen)
 
 		return 3648
@@ -127,14 +136,28 @@ class AppWindow(QtGui.QMainWindow, template_graph_nofft.Ui_MainWindow,utilitiesC
 				self.I.__fetch_channel__(1)
 				self.Y+=self.I.achans[0].get_yaxis()[32:-14]
 
-			x = range(3648)
-			self.curveCH1.setData(x,self.Y/self.num,connect='finite')
+			x = range(3648,0,-1)
+			self.Y = self.Y/self.num
+			self.Y = 3.3-self.Y
+			
+			if self.setRefFlag:
+				self.setRefFlag = False
+				self.REFY = self.Y
+			else:
+				if self.absorbance.isChecked():
+					ref = np.copy(self.REFY)
+					ref[ref==0]=1
+					self.Y = np.log10(self.Y/ref)
+				else:
+					self.Y -= self.REFY
+			self.curveCH1.setData(x,self.Y,connect='finite')
 			#print (x,self.Y/self.num)
 		except Exception as b:
 			#print (b)
 			pass
 
 		if self.running:self.timer.singleShot(200,self.run)
+
 
 	def saveData(self):
 		self.saveDataWindow([self.curveCH1],self.plot)
